36e888938f9f64797b07716704df88a22ccc0a9f
MarinaBajic <marinabajic10@gmail.com>
2024-08-13 20:10:09 +0200

    Added api gateway

---
+++ backend/api-gateway/src/main/java/com/reo/api_gateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,20 @@
+package com.reo.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/api/horses/**").uri("lb://horse-service"))
+                .route(p -> p.path("/api/riders/**").uri("lb://rider-service"))
+                .route(p -> p.path("/api/favorites/**").uri("lb://rider-service"))
+                .route(p -> p.path("/api/sessions/**").uri("lb://rider-service"))
+                .build();
+    }
+}
