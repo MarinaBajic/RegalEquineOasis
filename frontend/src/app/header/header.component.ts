ce752707c7705ba11d2e08e87fd75327e07a3ba5
MarinaBajic <marinabajic10@gmail.com>
2024-08-15 19:08:14 +0200

    Added header

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
