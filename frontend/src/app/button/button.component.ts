5fccd79b100838205d107ef5fe4d5530d0dd9fbc
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 00:52:14 +0200

    Primary and secondary btn

--- frontend/src/app/button/button.component.ts
+++ frontend/src/app/button/button.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
 })
 export class ButtonComponent {
   @Input() text = '';
+  @Input({ transform: booleanAttribute }) isPrimary: boolean = true;
 }
