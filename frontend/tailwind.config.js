f819a6e14a3a99a147e0f8026c5dabdbff4c6a82
MarinaBajic <marinabajic10@gmail.com>
2024-08-15 19:07:35 +0200

    Added colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#eab308",
+        "accent-light": "#facc15",
+        dark: "#333",
+        light: "#f2f2f2",
+      },
+    },
   },
   plugins: [],
 };
