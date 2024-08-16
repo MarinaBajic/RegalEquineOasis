adc807bec478f777664181002bdcb0d8b714e301
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:06:49 +0200

    Added horses component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HorsesComponent } from './components/horses/horses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, HorsesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
