3774b69966ff8d451fdaf2593cd6f9c24aa1ce3c
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:01:34 +0200

    Added horse card component

---
+++ frontend/src/app/components/horse-card/horse-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horse-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './horse-card.component.html',
+})
+export class HorseCardComponent {
+
+}
