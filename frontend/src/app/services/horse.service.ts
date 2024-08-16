40feda96ded8b0e573cc57fb91a9b1ddbf2ea3fd
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:01:04 +0200

    Added HttpClient

--- frontend/src/app/services/horse.service.ts
+++ frontend/src/app/services/horse.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Horse } from '../model/Horse';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HorseService {
+  private baseUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.baseUrl = 'http://localhost:8765/api';
+  }
+
+  getHorses(): Observable<Array<Horse>> {
+    return this.http.get<Array<Horse>>(`${this.baseUrl}/horses`);
+  }
 }
