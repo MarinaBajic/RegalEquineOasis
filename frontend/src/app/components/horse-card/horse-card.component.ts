e52854f72faaa3c8f0872d4dc45b83f850cee684
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:56:54 +0200

    Display horses

--- frontend/src/app/components/horse-card/horse-card.component.ts
+++ frontend/src/app/components/horse-card/horse-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Horse } from '../../model/Horse';
 
 @Component({
   selector: 'app-horse-card',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   templateUrl: './horse-card.component.html',
 })
 export class HorseCardComponent {
-
+  @Input()
+  horse!: Horse;
 }
--- frontend/src/app/components/horses/horses.component.ts
+++ frontend/src/app/components/horses/horses.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { HorseService } from '../../services/horse.service';
 import { Observable } from 'rxjs';
 import { Horse } from '../../model/Horse';
+import { HorseCardComponent } from '../horse-card/horse-card.component';
+import { AsyncPipe, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-horses',
   standalone: true,
-  imports: [],
+  imports: [HorseCardComponent, AsyncPipe, NgForOf],
   templateUrl: './horses.component.html',
 })
 export class HorsesComponent {
-  horses: Observable<Array<Horse>> | undefined;
+  horses!: Observable<Array<Horse>>;
 
   constructor(private horseService: HorseService) {}
 
