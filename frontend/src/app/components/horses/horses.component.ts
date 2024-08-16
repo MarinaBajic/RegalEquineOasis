0b3e31db510a07f45e85c33895115fed27732ea7
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:06:37 +0200

    Added fetch

--- frontend/src/app/components/horses/horses.component.ts
+++ frontend/src/app/components/horses/horses.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HorseService } from '../../services/horse.service';
+import { Observable } from 'rxjs';
+import { Horse } from '../../model/Horse';
 
 @Component({
   selector: 'app-horses',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   templateUrl: './horses.component.html',
 })
 export class HorsesComponent {
+  horses: Observable<Array<Horse>> | undefined;
 
+  constructor(private horseService: HorseService) {}
+
+  ngOnInit() {
+    this.fetchHorses();
+  }
+
+  fetchHorses() {
+    this.horses = this.horseService.getHorses();
+  }
 }
