8a6a99e13d9184f24033d5c77ff50cdd4a0370e2
MarinaBajic <marinabajic10@gmail.com>
2024-08-16 18:01:42 +0200

    Added horses component

---
+++ frontend/src/app/components/horses/horses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horses',
+  standalone: true,
+  imports: [],
+  templateUrl: './horses.component.html',
+})
+export class HorsesComponent {
+
+}
