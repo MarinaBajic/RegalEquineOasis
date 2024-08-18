0a59b6f7c159b572f8005d02abf3e90b2163ff71
MarinaBajic <marinabajic10@gmail.com>
2024-08-18 19:19:40 +0200

    FE: styled about section

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
