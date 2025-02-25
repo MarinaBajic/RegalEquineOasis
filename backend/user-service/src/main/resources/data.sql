5c58f08dc2c919bdcbe388822f7121e4ed33ec9a
MarinaBajic <marinabajic10@gmail.com>
2025-02-26 00:29:18 +0100

    BE: changed user password

--- backend/user-service/src/main/resources/data.sql
+++ backend/user-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- Populate User table
 INSERT INTO user (username, password, role) VALUES
 ('duki', '$2a$12$mXxSLC7w.7aqdYCrkC5GC.uhLa8fPxRLmbFz7XZU0evmZFRJyGA1e', 0),
-('maki', '$2a$12$mXxSLC7w.7aqdYCrkC5GC.uhLa8fPxRLmbFz7XZU0evmZFRJyGA1e', 1);
+('maki', '$2a$12$GkhowGLsuG6L1M7ga1sKEeFwZ.t1gYEJWLssa8t0VHoAgkpTwXkOa', 1); --maki
